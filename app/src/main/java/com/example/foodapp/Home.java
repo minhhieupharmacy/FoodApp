71a929c1649dc801cd3ed9b8dee920a7db418762
minhhieupharmacy <45650781+minhhieupharmacy@users.noreply.github.com>
2019-08-23 20:00:34 +0700

    Update Home.java

--- app/src/main/java/com/example/foodapp/Home.java
+++ app/src/main/java/com/example/foodapp/Home.java
@@ -4,6 +4,7 @@ public class Home {
     String name ;
     String address;
     int images;
+    //tao ghi chu
     public Home(String name, String address, int images) {
         this.name = name;
         this.address = address;
